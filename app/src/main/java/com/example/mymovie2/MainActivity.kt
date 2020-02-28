dbd68b384dc2ec2e72073c512005299041be5e7e
KJH <jinhungkim@hanmail.net>
2020-02-28 18:11:06 +0900

    NEW

--- app/src/main/java/com/example/mymovie2/MainActivity.kt
+++ app/src/main/java/com/example/mymovie2/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.mymovie2
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_recycler.*
 import kotlinx.android.synthetic.main.main.*
 import kotlinx.android.synthetic.main.review.*
@@ -16,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     var totalUpCount: Int = 15
     var totalDownCount: Int = 1
+    var story = "<b>줄거리</b> <br><br>군도, 백성을 구하라<br>양반과 탐관오리들의 착취가 극에 달했던 조선철종 13년.<br>힘 없는 백성의 편이 되어 세상을 바로 잡고자 하는 의적떼인 군도. 지리산 추설이 있었다. <br><br>쌍칼 도치 vs 백성의 적 조윤<br>잦은 자연재해, 기근과 관의 횡포까지 겹쳐 백성들의 삶이 날로 피폐해 져 가는 사이, 나주 대부호의 서자로 조선 최고의 무관 출신인 조윤은 극악한 수법으로 양민들을 수탈, 삼남지방 최고의 대부호로 성장한다."
 
     // 선택여부 ( 미선택: 0, up 선택: 1, down 선택: 2)
     var cheakSelect: Int = 0
@@ -38,6 +42,15 @@ class MainActivity : AppCompatActivity() {
         }
         tv_up.setText(totalUpCount.toString())
         tv_down.setText(totalDownCount.toString())
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            tv_story.setText(Html.fromHtml(story, Html.FROM_HTML_MODE_LEGACY))
+        } else {
+            tv_story.setText(Html.fromHtml(story))
+        }
+
+
 
 
         // 버튼 이벤트
